1a4c54b296b95ac106cf88b29785801fc1b5cd61
JulianafBandeira <julianafbbarbosa@gmail.com>
2023-10-06 19:15:50 -0300

    config json

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 
-async function fetchProfileData() {
+async function fetchProfileData(){
     const url = 'https://github.com/JulianafBandeira/portfolio/blob/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
